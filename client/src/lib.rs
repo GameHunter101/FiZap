c95fb69326f0ad51f5736e987c76fc3d1115ac66
GameHunter101 <liorscarmeli@gmail.com>
2023-06-20 18:59:52 -0400

    Server side rendering (pain)

---
+++ client/src/lib.rs
@@ -0,0 +1,47 @@
+use std::collections::HashMap;
+
+use gloo_net::http::Request;
+use wasm_bindgen_futures::spawn_local;
+use yew::prelude::*;
+use yew_router::{
+    history::{AnyHistory, History, MemoryHistory},
+    prelude::*,
+};
+
+#[derive(Clone, Routable, PartialEq)]
+enum Route {
+    #[at("/test")]
+    Home,
+}
+
+#[derive(Properties, PartialEq, Eq, Debug)]
+pub struct ServerAppProps {
+    pub url: AttrValue,
+    pub queries: HashMap<String, String>,
+}
+
+fn switch(routes: Route) -> Html {
+    match routes {
+        Route::Home => html! {<h1>{"Hello frontend"}</h1>},
+    }
+}
+
+#[function_component(App)]
+pub fn app() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={switch} />
+        </BrowserRouter>
+    }
+}
+
+#[function_component(ServerApp)]
+pub fn server_app(props: &ServerAppProps) -> Html {
+    let history = AnyHistory::from(MemoryHistory::new());
+    history.push(&*props.url);
+    html! {
+        <Router history={history}>
+            <Switch<Route> render={switch} />
+        </Router>
+    }
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,24 +1,35 @@
+use std::collections::HashMap;
+use std::convert::Infallible;
 use std::env;
 use std::net::{IpAddr, Ipv4Addr, SocketAddr};
+use std::path::PathBuf;
 use std::str::FromStr;
 use std::sync::Arc;
 
-use axum::extract::State;
+use axum::body::StreamBody;
+use axum::error_handling::HandleError;
+use axum::extract::{Query, State};
+use axum::http::{Request, StatusCode};
 use axum::middleware::from_fn_with_state;
-use axum::Extension;
 use axum::{
+    body::Body,
     response::{Html, IntoResponse},
     routing::get,
     routing::post,
     Router,
 };
+use axum::{routing, Extension};
 use clap::Parser;
+use client::{ServerApp, ServerAppProps};
 use dotenv::dotenv;
+use futures::stream::{self, StreamExt};
 use mongodb::bson::Document;
-use mongodb::{Collection, Client};
 use mongodb::options::{ClientOptions, ResolverConfig};
-use tower::ServiceBuilder;
+use mongodb::{Client, Collection};
+use tower::{ServiceBuilder, ServiceExt};
+use tower_http::services::ServeDir;
 use tower_http::trace::TraceLayer;
+use yew::platform::Runtime;
 
 use crate::middleware::authentication_middleware;
 use crate::routes::auth::{login, signup};
@@ -28,9 +39,9 @@ mod routes {
 }
 mod middleware;
 
-#[derive(Parser, Debug)]
+#[derive(Parser, Debug, Clone)]
 #[clap(name = "server", about = "A file hosting server")]
-struct Opt {
+pub struct Opt {
     #[clap(short = 'l', long = "log", default_value = "debug")]
     log_level: String,
 
@@ -40,7 +51,7 @@ struct Opt {
     #[clap(short = 'p', long = "port", default_value = "8080")]
     port: u16,
 
-    #[clap(long = "static-dir", default_value = "../client/dist")]
+    #[clap(long = "static-dir", default_value = "./dist")]
     static_dir: String,
 }
 
@@ -77,23 +88,28 @@ impl Config {
 pub struct AppState {
     pub config: Config,
     pub user_collection: Collection<Document>,
+    pub opt: Opt,
 }
 
 impl AppState {
     async fn init() -> AppState {
         let config = Config::init();
-        let client_options =
-            ClientOptions::parse_with_resolver_config(&config.mongodb_uri, ResolverConfig::cloudflare())
-                .await
-                .unwrap();
+        let client_options = ClientOptions::parse_with_resolver_config(
+            &config.mongodb_uri,
+            ResolverConfig::cloudflare(),
+        )
+        .await
+        .unwrap();
         let client = Client::with_options(client_options).unwrap();
-    
+
         let user_collection = client
             .database("MuZap")
             .collection::<mongodb::bson::Document>("users");
+        let opt = Opt::parse();
         AppState {
             config,
             user_collection,
+            opt,
         }
     }
 }
@@ -102,42 +118,72 @@ impl AppState {
 async fn main() {
     dotenv().ok();
 
-    let opt = Opt::parse();
+    let state = AppState::init().await;
 
     if std::env::var("RUST_LOG").is_err() {
-        std::env::set_var("RUST_LOG", format!("{},hyper=info,mio=info", opt.log_level))
+        std::env::set_var(
+            "RUST_LOG",
+            format!("{},hyper=info,mio=info", &state.opt.log_level),
+        )
     }
 
     tracing_subscriber::fmt::init();
 
-    let state = AppState::init().await;
+    let addr = SocketAddr::from((
+        IpAddr::from_str(&state.opt.addr).unwrap_or(IpAddr::V4(Ipv4Addr::LOCALHOST)),
+        state.opt.port,
+    ));
 
     let app = Router::new()
         .route("/", get(root))
-        .route("/login", get(login))
-        .route("/signup", post(signup))
         .nest(
             "/api",
             Router::new()
+                .route("/login", get(login))
+                .route("/signup", post(signup))
                 .route("/test", get(handler))
-                .route_layer(from_fn_with_state(
-                    state.clone(),
-                    authentication_middleware,
-                )),
+                .route_layer(from_fn_with_state(state.clone(), authentication_middleware)),
         )
+        .fallback(render)
         .layer(ServiceBuilder::new().layer(TraceLayer::new_for_http()))
         .with_state(Arc::new(state));
-
-    let addr = SocketAddr::from((
-        IpAddr::from_str(&opt.addr).unwrap_or(IpAddr::V4(Ipv4Addr::LOCALHOST)),
-        opt.port,
-    ));
 
     log::info!("Listening on http://{}", addr);
     axum::Server::bind(&addr)
         .serve(app.into_make_service())
         .await
         .unwrap();
+}
+
+async fn render(
+    Query(queries): Query<HashMap<String, String>>,
+    State(state): State<Arc<AppState>>,
+    url: Request<Body>,
+) -> impl IntoResponse {
+    let index_html_full =
+        tokio::fs::read_to_string(PathBuf::from(&state.opt.static_dir).join("index.html"))
+            .await
+            .expect("Failed to read index.html");
+    let index_html_split = index_html_full.split("body").collect::<Vec<_>>();
+    let mut index_html_before = index_html_split[0].to_owned();
+    index_html_before.push_str("body>");
+
+    let mut index_html_after = "</body".to_owned();
+    index_html_after.push_str(index_html_split[2]);
+    
+    let url = url.uri().to_string();
+
+    let renderer = yew::ServerRenderer::<ServerApp>::with_props(move || ServerAppProps {
+        url: url.into(),
+        queries,
+    });
+
+    StreamBody::new(
+        stream::once(async move { index_html_before })
+            .chain(renderer.render_stream())
+            .chain(stream::once(async move { index_html_after }))
+            .map(Result::<_, Infallible>::Ok),
+    )
 }
 
 async fn root(State(state): State<Arc<AppState>>) -> impl IntoResponse {
